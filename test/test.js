1bc9a9b690dcfb972a29417ba1bb75488d062255
daniel <dyrda@in.tum.de>
2018-06-18 12:40:20 +0200

    added 'empty' and 'push' tests

--- test/test.js
+++ test/test.js
@@ -1,1 +1,44 @@
 import test from 'ava';
+import {RuntimeTopicStorage} from './../dist/js/bundle.js';
+
+(function(){
+
+	
+	let createStorageSnapwhotOne = function(){
+		let raw = {};
+
+		return raw;
+    }
+    
+    let createStorageSnapwhotTwo = function(){
+		let raw = {
+            't:a': {
+                'd:data': {},
+            }
+        };
+
+		return raw;
+	}
+
+
+	test('empty', t => {
+        let snapshot = createStorageSnapwhotOne();
+        
+        let storage = new RuntimeTopicStorage();
+
+        t.deepEqual(storage.storage, snapshot);
+
+    });
+    
+    test('push', t => {
+        let snapshot = createStorageSnapwhotTwo();
+        
+        let storage = new RuntimeTopicStorage();
+
+        storage.push('a',{});
+
+        t.deepEqual(storage.storage, snapshot);
+
+	});
+
+})();
