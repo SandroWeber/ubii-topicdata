74d59687185b3b363b05218b870c37e62dee6243
daniel <dyrda@in.tum.de>
2018-07-09 18:03:12 +0200

    refactoring process

--- src/topicData/runtimeTopicData.js
+++ src/topicData/runtimeTopicData.js
@@ -4,16 +4,15 @@ const {getTopicPathFromArray} = require('./utility.js');
 
 (function(){
   /**
-   * Local runtime implementaion of a topic storage.
+   * Local runtime implementaion of a topic data.
    * The data is only available at runtime and is not permanently stored.
    * (No local file or database involved. The data is only in the program memory.)
+   * 
+   * The runtime topic storage uses a common javascript object as storage structure.
+   * This is the most performant way to find key-value pairs.
+   * The topic of queries is split and used as property keys.
    */
   class RuntimeTopicData extends TopicData {
-    /*
-    * The runtime topic storage uses a common javascript object as storage structure.
-    * This is the most performant way to find key-value pairs.
-    * The topic of queries is split and used as property keys.
-    * */
     
     constructor() {
       super();
@@ -22,18 +21,18 @@ const {getTopicPathFromArray} = require('./utility.js');
     }
 
     /**
-     * Pushes data into memory under the specified topic.
+     * Pushes data under the specified topic into the topic data
      * If there is already data under this topic, it will be overwritten.
      * @param {String[]} topic Array of unprefixed subtopic strings specifying the topic path.
      * @param {*} data 
      */
     push(topic, data){
-      let subtreeRoot = traverseAndCreateTopicPath.call(this, topic);
-      subtreeRoot[DATA_PROPERTY_KEY] = data;
+      let node = getTopicSubtree.call(this, topic);
+      node[DATA_PROPERTY_KEY] = data;
     }
 
     /**
-     * Pulls the data from memory that is stored under the given topic.
+     * Pulls the data from topic data that is stored under the given topic.
      * @param {String[]} topic Array of unprefixed subtopic strings specifying the topic path.
      * @returns Returns the data stored under the given topic. Returns undefined if the topic does not exist.
      */
@@ -41,12 +40,12 @@ const {getTopicPathFromArray} = require('./utility.js');
       if(!this.has(topic)){
         return undefined;
       }
-      let subtreeRoot = traverseAndCreateTopicPath.call(this, topic);
-      return subtreeRoot[DATA_PROPERTY_KEY];
+      let node = getTopicSubtree.call(this, topic);
+      return node[DATA_PROPERTY_KEY];
     }
 
     /**
-     * Removes the topic and data from memory that is stored under the given topic if the topic exists. 
+     * Removes the topic and data from topic data that is stored under the given topic if the topic exists. 
      * Cleans up the path afterwards.
      * @param {String[]} topic Array of unprefixed subtopic strings specifying the topic path.
      */
@@ -54,10 +53,10 @@ const {getTopicPathFromArray} = require('./utility.js');
       if(!this.has(topic)){
         return;
       }
-      let subtreeRoot = traverseAndCreateTopicPath.call(this,topic);
-      delete subtreeRoot[DATA_PROPERTY_KEY];
+      let node = getTopicSubtree.call(this,topic);
+      delete node[DATA_PROPERTY_KEY];
 
-      cleanPath.call(this,topic);
+      cleanUpPath.call(this,topic);
     }
 
     /**
@@ -66,7 +65,6 @@ const {getTopicPathFromArray} = require('./utility.js');
      * @returns {Boolean} Returns whether the the storage has the given topic or not.
      */
     has(topic){
-      // get topic path
       const path = getTopicPathFromArray(topic);
 
       // traverse path
@@ -86,12 +84,12 @@ const {getTopicPathFromArray} = require('./utility.js');
   // --- private methods
 
   /**
-   * 
+   * Retruns the subtree with the specified topic as root.
+   * If the spcified topic does not exist, its topic path is created if createOnTraverse is true.
    * @param {String[]} topic Array of unprefixed subtopic strings specifying the topic path.
-   * @return returns the path target subtree
+   * @return returns the subtree of the topic path target
    */
-  let traverseAndCreateTopicPath = function(topic){
-    // get topic path
+  let getTopicSubtree = function(topic, createOnTraverse = true){
     const path = getTopicPathFromArray(topic);
 
     // traverse path and create if necessary
@@ -102,15 +100,24 @@ const {getTopicPathFromArray} = require('./utility.js');
       if(subtree.hasOwnProperty(path[i])){
         subtree = subtree[path[i]];
       }else{
-        subtree[path[i]] = {};
-        subtree = subtree[path[i]];
+        if(createOnTraverse){
+          subtree[path[i]] = {};
+          subtree = subtree[path[i]];
+        }else{
+          subtree = undefined;
+          break;
+        }
       }
     }
 
     return subtree;
   }
 
-  let cleanPath = function(topic){
+  /**
+   * Removes all properties that do not have any data key in its subtree.
+   * @param {String[]} topic Array of unprefixed subtopic strings specifying the topic path.
+   */
+  let cleanUpPath = function(topic){
     const path = getTopicPathFromArray(topic);
 
     if(!recursiveIsRelevantCleanUp(this.storage[path[0]])){
@@ -127,7 +134,6 @@ const {getTopicPathFromArray} = require('./utility.js');
       if(keys[i] === DATA_PROPERTY_KEY){
         rootIsRelevant = true;
       }else{
-        // ToDo: Check for valid path subtree (starts with t:)
         let subtreeIsReleveant = recursiveIsRelevantCleanUp(subtree[keys[i]]);
         if(!subtreeIsReleveant){
           delete subtree[keys[i]];
@@ -140,6 +146,5 @@ const {getTopicPathFromArray} = require('./utility.js');
 
 
   module.exports = RuntimeTopicData;
-
 })();
 
