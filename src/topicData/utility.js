4033811232ec505ca96ae8856c4aaaa89263642f
Daniel <dyrda@in.tum.de>
2018-08-15 13:42:12 +0200

    Changed validateTopic method to match topic is a single string policy

--- src/topicData/utility.js
+++ src/topicData/utility.js
@@ -9,18 +9,9 @@ let getTopicPathFromArray = function (topic) {
 }
 
 let validateTopic = function(topic){
-    // topic is an array
-    if(!Array.isArray(topic)){
-        throw new Error('Topic is not valid: Topic is no array. '+topic);
-    }
-
-    // topic is an array of strings
-    let i, il;
-    il = topic.length;
-    for(i = 0; i < il; i++){
-        if (!(typeof topic[i] === 'string' || topic[i] instanceof String)){
-            throw new Error('Topic is not valid: Not all elements of the topic array are strings. '+topic+' at index '+i+': '+topic[i]);
-        }
+    //topic is a string
+    if (!(typeof topic === 'string' || topic instanceof String)){
+        throw new Error('The specified topic is not valid: The topic is not a string: '+topic);
     }
 }
 
