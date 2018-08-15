4f455319f81fc4eb4838987239649e2525799e17
Daniel <dyrda@in.tum.de>
2018-08-15 14:09:20 +0200

    Fixed typo and added suffic explanation

--- src/topicData/constants.js
+++ src/topicData/constants.js
@@ -1,6 +1,6 @@
 const DATA_PREFIX = 'd:';
 const TOPIC_PREFIX = 't:';
-const TOPIC_SUFFIX = ':t';
+const TOPIC_SUFFIX = ':t';  // Topic suffix allows the use of sub-topics with spaces at the end without encouraging bugs and human error.
 const SUBSCRIBER_PREFIX = 's:';
 const DATA_PROPERTY_KEY = DATA_PREFIX + 'data';
 const SUBSCRIBER_PROPERTY_KEY = SUBSCRIBER_PREFIX + 'subscriber';
@@ -8,7 +8,7 @@ const TOPIC_SEPARATOR = '->';
 
 module.exports = {
     TOPIC_PREFIX: TOPIC_PREFIX,
-    TOPIC_SUFIX: TOPIC_SUFIX,
+    TOPIC_SUFFIX: TOPIC_SUFFIX,
     DATA_PREFIX: DATA_PREFIX,
     SUBSCRIBER_PREFIX: SUBSCRIBER_PREFIX,
     DATA_PROPERTY_KEY: DATA_PROPERTY_KEY,
