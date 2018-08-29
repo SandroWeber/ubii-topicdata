4146752c49b607ab454a59cc080f7c893369d05c
Daniel <dyrda@in.tum.de>
2018-08-29 12:47:25 +0200

    getTopicPathFromString removes all empty subtopic strings now

--- src/topicData/utility.js
+++ src/topicData/utility.js
@@ -4,8 +4,12 @@ const {
     TOPIC_SEPARATOR
 } = require('./constants.js');
 
+/**
+ * Creates a path array from a topic string. Removes all empty subtopic strings and adds the topic prefix and suffix to each subtopic of the path array.
+ * @param {String} topicString 
+ */
 let getTopicPathFromString = function (topicString) {
-    return topicString.toString().split(TOPIC_SEPARATOR).map(t => '' + TOPIC_PREFIX + t + TOPIC_SUFFIX);
+    return topicString.toString().split(TOPIC_SEPARATOR).filter(t => t!=='').map(t => '' + TOPIC_PREFIX + t + TOPIC_SUFFIX);
 }
 
 let validateTopic = function (topic) {
